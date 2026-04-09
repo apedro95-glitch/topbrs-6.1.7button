9d8280877211d2f5b41edeb97af2870cca67c21a
apedro95-glitch <apedro.95@icloud.com>
2026-04-09 19:42:22 -0300

    Atualizar o firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 window.TOPBRS_FIREBASE_CONFIG = {
   enabled: true,
-  firebaseConfig: {
-  apiKey: "AIzaSyBmrJTQwmS9cYZqH4IlZlPS6SxY4hG7quk",
-  authDomain: "topbrs.firebaseapp.com",
-  projectId: "topbrs",
-  storageBucket: "topbrs.firebasestorage.app",
-  messagingSenderId: "608140308336",
-  appId: "1:608140308336:web:657fcbdd3ed9b1367be9b5",
-  measurementId: "G-CD5H4NL802"
-},
+  firebaseConfig = {
+  apiKey: "AIzaSyDYWlJNOhBoQOgX_qTNd52PnpWXYLGzxbs",
+  authDomain: "topbrsv1.firebaseapp.com",
+  projectId: "topbrsv1",
+  storageBucket: "topbrsv1.firebasestorage.app",
+  messagingSenderId: "266244923192",
+  appId: "1:266244923192:web:bdd29755e33bd687d92bcd",
+  measurementId: "G-N0NVGD9N35"
+};
   adminEmail: "silva12.anderson@gmail.com",
   stateDocPath: "live/topbrs_state"
 };
